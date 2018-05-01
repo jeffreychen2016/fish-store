175073a695d85126fe2c4f8afdd2ffd66f14b4e7
Xuehui Chen <jeffreychen2016@gmail.com>
2018-05-01 18:36:02 -0500

    added browserify

---
+++ javascripts/events.js
@@ -0,0 +1,34 @@
+// Filter fish that are "on sale"
+const filterFish = () => {
+  $('#available .fish').not('.on-sale').toggle();
+};
+
+const changeButtonText = () => {
+  $('#show-sale').text((i,text) => {
+    let returnText = '';
+    if (text === 'Show Sale Fish') {
+      returnText = 'Show All';
+    } else {
+      returnText = 'Show Sale Fish';
+    };
+    return returnText;
+  });
+};
+
+// Add fish to "Basket"
+const moveToCart = (e) => {
+  // it will move the element to the destination, no need to delete the old element
+  const fishCard = $(e.target).closest('.fish');
+  // console.log('fishcard',fishCard);
+  $('#snagged').append(fishCard);
+};
+
+const bindEvents = () => {
+  $('button.add').click(moveToCart);
+  $('#show-sale').click(() => {
+    changeButtonText();
+    filterFish();
+  });
+};
+
+module.exports = bindEvents;
